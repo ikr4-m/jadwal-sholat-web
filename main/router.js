f3ebaf93d552e589a219623c4e08bbfb74066320
Ikramullah Latif <sky_m4n@outlook.com>
2018-11-17 15:21:34 +0800

    Change router and fix hadist-integral
    1 s = 1000 ms

--- main/router.js
+++ main/router.js
@@ -3,9 +3,6 @@ module.exports = (app, site, router, logger) => {
         app.get('/', function (req, res) {
             res.render(".." + site + 'index.html');
         });
-        app.get('/test/anima', function (req, res) {
-            res.render(".." + site + 'test/anima.html');
-        });
     } catch (error) {
         console.log(logger("ERROR") + `${error.message}\n${error}`);
     } finally {
--- public/js/rand-hadist.js
+++ public/js/rand-hadist.js
@@ -1,5 +1,5 @@
 var hadist = hadist_list;
-var hadistIntegral = (2 * 60) * 100;
+var hadistIntegral = (1.5 * 60) * 1000;
 $().ready(() => {
     function reloadHadist(array) {
         return Math.floor(Math.random() * array.length);
